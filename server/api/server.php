cb8c31dfd00b8bd764a7c2cf6a7e727964f993de
calogero <calo19296@gmail.com>
2020-11-09 20:26:30 +0100

    setup server and test

---
+++ server/api/server.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+
+
+$msg = "Invalid API!";
+if(isset($_GET['url'])){
+    $var = $_GET['url'];
+    $value ="undefined";
+    $number = intval(preg_replace('/[^0-9]+/', '', $var));
+    switch($var){
+        case "users":
+            $value = "users";
+            $requestMethod = $_SERVER['REQUEST_METHOD'];
+            echo "call controller user";
+        break;
+        case "users/$number":
+            $value = "users";
+            $requestMethod = $_SERVER['REQUEST_METHOD'];
+            echo "call controller user with id = ".$number;
+        break;
+        default:
+            echo $msg;
+
+    }
+}
+
+
+?>
