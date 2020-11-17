89a63e6b64b053381579478baeecdf9395dcd14c
Rocco Petruzzi <s280126@studenti.polito.it>
2020-11-17 23:33:18 +0100

    [PULSBS-38] add API endpoint

--- serverBuild/server/src/api/Server.php
+++ serverBuild/server/src/api/Server.php
@@ -1,62 +1,63 @@
 <?php
 header('Content-Type: application/json');
 require '../../vendor/autoload.php';
-if(isset($_SERVER['REQUEST_METHOD'])){
-    if($_SERVER['HTTP_USER_AGENT'] == 'GuzzleHttp/7'){
+if (isset($_SERVER['REQUEST_METHOD'])) {
+    if ($_SERVER['HTTP_USER_AGENT'] == 'GuzzleHttp/7') {
         $dbConn = new \SQLite3("../../tests/dbForTesting2.db");
-    }
-    else{
+    } else {
         $dbConn = new \SQLite3("../db.db");
     }
-    
+
 }
 
-
 $msg = "Invalid API!";
-if(isset($_GET['url'])){
+if (isset($_GET['url'])) {
     $var = $_GET['url'];
-    $value ="undefined";
+    $value = "undefined";
     $number = intval(preg_replace('/[^0-9]+/', '', $var));
-    switch($var){
+    switch ($var) {
         case "bookableLessons/$number":
-            $value ="bookableLessons";
+            $value = "bookableLessons";
             $requestMethod = "GET";
-            $id = $number; 
+            $id = $number;
             $controller = new Server\api\ControllersStudentBooking($requestMethod, $dbConn, $value, $id);
             $controller->processRequest();
-        break;
+            break;
         case "insertLecture":
             $value = "insertLecture";
             $requestMethod = "POST";
             $controller = new Server\api\ControllersStudentBooking($requestMethod, $dbConn, $value);
             $controller->processRequest();
-        break;
+            break;
         case "studentBookings/$number":
             $value = "studentBookings";
             $requestMethod = "GET";
-            $id = $number; 
-            $controller = new Server\api\ControllersStudentBooking($requestMethod, $dbConn, $value, $id);
-            $controller->processRequest();
-        break;
-        case "updateBooking/$number":
-            $value = "updateBooking";
-            $requestMethod =  "PUT";
             $id = $number;
             $controller = new Server\api\ControllersStudentBooking($requestMethod, $dbConn, $value, $id);
             $controller->processRequest();
-        break;
+            break;
+        case "updateBooking/$number":
+            $value = "updateBooking";
+            $requestMethod = "PUT";
+            $id = $number;
+            $controller = new Server\api\ControllersStudentBooking($requestMethod, $dbConn, $value, $id);
+            $controller->processRequest();
+            break;
         case "bookedStudentsForLecture/$number":
             $value = "bookedStudentsForLecture";
             $requestMethod = "GET";
             $id = $number;
             $controller = new Server\api\ControllersTeacherBooking($requestMethod, $dbConn, $value, $id);
             $controller->processRequest();
-        break;
+            break;
+        case "sendNotification":
+            $value = "sendNotification";
+            $requestMethod = $_SERVER['REQUEST_METHOD'];
+            $controller = new Server\api\ControllersNotification($requestMethod, $dbConn, $value);
+            $controller->processRequest();
+            break;
         default:
             echo $msg;
 
     }
 }
-
-
-?>
