87f9cca4169f20ad5a6563d7f89c0a863e4777e8
Unknown <s267562@studenti.polito.it>
2020-11-28 17:37:44 +0100

    bm page upgrade

--- clientBuild/client/src/BookingManagerPage.js
+++ clientBuild/client/src/BookingManagerPage.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react';
 import ApexChart from "react-apexcharts";
 import {ButtonGroup, ToggleButton, Container} from 'react-bootstrap'; 
+import Select from 'react-select';
+
+const courses = [
+    {idCourse:3,courseName:"Geometry"},
+    {idCourse:2,courseName:"Software Engineering II"},
+    {idCourse:5,courseName:"Computer Science"},
+] 
 
 class BookingManagerPage extends React.Component {
 
@@ -8,6 +15,8 @@ class BookingManagerPage extends React.Component {
       super(props);
 
       this.state = {
+
+        selectedCourse: "",
 
         series: [{
           name: 'Net Profit',
@@ -28,7 +37,7 @@ class BookingManagerPage extends React.Component {
           plotOptions: {
             bar: {
               horizontal: false,
-              columnWidth: '25%',
+              columnWidth: '50%',
             },
           },
           dataLabels: {
@@ -59,6 +68,40 @@ class BookingManagerPage extends React.Component {
           }
         },
         
+        series2: [{
+            name: "Desktops",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }],
+
+        options2:{
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'straight'
+            },
+            title: {
+              text: 'Product Trends by Month',
+              align: 'left'
+            },
+            grid: {
+              row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+              },
+            },
+            xaxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+            }
+          },
+
       };
     }
 
@@ -66,24 +109,49 @@ class BookingManagerPage extends React.Component {
         return <>
         <Container fluid>
             <br />
-            <h1>Historical data</h1>
+            <h1 className="text-center">Historical data</h1>
+            
+            <ApexChart className="mt-4 col-6 mx-auto" options={this.state.options} series={this.state.series} type="bar" height={430} />
 
-            <MultiStateToggleButton
-                radios={[ { name: 'Month', value: '1' }, { name: 'Week', value: '2' }, { name: 'Lecture', value: '3' } ]} 
+            <br />
+            <hr />
+
+            <Select 
+                className="mt-4 col-4 mx-auto"
+                options={courses}
+                placeholder="Select a course..." 
+                getOptionLabel={(course)=>course.courseName}
+                getOptionValue={(course)=>course.idCourse}
+                onChange={selection => this.handleSelectionChange(selection)} 
                 />
+                
+            <br />
 
-            <div id="chart">
-                <ApexChart options={this.state.options} series={this.state.series} type="bar" height={430} />
-            </div>
+            {
+            this.state.selectedCourse !== "" ?
+                <>
+                    <ApexChart className="mt-4 col-6 mx-auto" options={this.state.options2} series={this.state.series2} type="line" height={350} />                
+                </>
+                :
+                <></> /* no course selected */
+            }
+
         </Container>
         </>
     }
 
-    handleListClick = (newDetail) => {
-        this.setState({ currentDetail: newDetail });
+    handleSelectionChange = (selection) => {
+        this.setState({ selectedCourse: selection.idCourse });
     }
 }
 
+
+/* 
+ * how to use? Here it is an example...
+ *   <MultiStateToggleButton
+ *       radios={[ { name: 'Month', value: '1' }, { name: 'Week', value: '2' }, { name: 'Lecture', value: '3' } ]} 
+ *   /> 
+ */
 function MultiStateToggleButton(props) {
     const [radioValue, setRadioValue] = useState('1');
   
@@ -91,21 +159,23 @@ function MultiStateToggleButton(props) {
   
     return (
       <>
-        <ButtonGroup toggle>
-          {radios.map((radio, idx) => (
-            <ToggleButton
-              key={idx}
-              type="radio"
-              variant="dark"
-              name="radio"
-              value={radio.value}
-              checked={radioValue === radio.value}
-              onChange={(e) => setRadioValue(e.currentTarget.value)}
-            >
-              {radio.name}
-            </ToggleButton>
-          ))}
-        </ButtonGroup>
+        <div className="text-center">
+            <ButtonGroup toggle>
+            {radios.map((radio, idx) => (
+                <ToggleButton
+                key={idx}
+                type="radio"
+                variant="dark"
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+                >
+                {radio.name}
+                </ToggleButton>
+            ))}
+            </ButtonGroup>
+        </div>
       </>
     );
 } 
