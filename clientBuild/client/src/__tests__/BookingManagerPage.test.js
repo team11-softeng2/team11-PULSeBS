55a943ac1eadf2587a0183311774e7511e0dcd8c
Fabio Stabile <s270121@studenti.polito.it>
2020-12-02 19:24:53 +0100

    Added BookingManagerPage tests

--- clientBuild/client/src/__tests__/BookingManagerPage.test.js
+++ clientBuild/client/src/__tests__/BookingManagerPage.test.js
@@ -1,6 +1,8 @@
 import TestRenderer from 'react-test-renderer';
 import BookingManagerPage from '../BookingManagerPage';
 import React from 'react';
+import API from '../API';
+import { shallow } from 'enzyme';
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -11,3 +13,27 @@ test('renders learn react link', () => {
     const tree = TestRenderer.create(<BookingManagerPage/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
+test('handleBookingFilterChange', async () => {
+  const component = shallow(<BookingManagerPage/>);
+  const instance = component.instance();
+  API.getBookingStatisticsByMonth = jest.fn(() => Promise.resolve([]));
+  API.getBookingStatisticsByWeek = jest.fn(() => Promise.resolve([]));
+  API.getBookingStatisticsByLesson = jest.fn(() => Promise.resolve([]));
+  await instance.handleBookingFilterChange("Month");
+  await instance.handleBookingFilterChange("Week");
+  await instance.handleBookingFilterChange("Lecture");
+  expect();
+});
+
+test('handleCancellationFilterChangee', async () => {
+  const component = shallow(<BookingManagerPage/>);
+  const instance = component.instance();
+  API.getCancellationsStatisticsByMonth = jest.fn(() => Promise.resolve([]));
+  API.getCancellationsStatisticsByWeek = jest.fn(() => Promise.resolve([]));
+  API.getCancellationsStatisticsByLesson = jest.fn(() => Promise.resolve([]));
+  await instance.handleCancellationFilterChange("Month");
+  await instance.handleCancellationFilterChange("Week");
+  await instance.handleCancellationFilterChange("Lecture");
+  expect();
+});
