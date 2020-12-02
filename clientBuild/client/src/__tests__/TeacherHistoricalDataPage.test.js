0f6d98cd441c75dfbe1daef233eed7b44ad3ed8d
Massimiliano Torchio <>
2020-12-02 11:24:53 +0100

    Test

--- clientBuild/client/src/__tests__/TeacherHistoricalDataPage.test.js
+++ clientBuild/client/src/__tests__/TeacherHistoricalDataPage.test.js
@@ -1,6 +1,6 @@
 import TestRenderer from 'react-test-renderer';
-import BookingManagerPage from '../BookingManagerPage';
 import React from 'react';
+import TeacherHistoricalDataPage from '../TeacherHistoricalDataPage';
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,6 +8,6 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 test('renders learn react link', () => {
-    const tree = TestRenderer.create(<BookingManagerPage/>).toJSON();
+    const tree = TestRenderer.create(<TeacherHistoricalDataPage/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
