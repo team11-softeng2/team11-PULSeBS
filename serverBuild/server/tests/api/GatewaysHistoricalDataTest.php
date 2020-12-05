af3fbbaf8f00ead1b24e4722cbd172a783520ed1
Rocco Petruzzi <s280126@studenti.polito.it>
2020-12-05 20:03:48 +0100

    add tests, refactor and technical debt (duplications removed)

--- serverBuild/server/tests/api/GatewaysHistoricalDataTest.php
+++ serverBuild/server/tests/api/GatewaysHistoricalDataTest.php
@@ -6,37 +6,42 @@ class GatewaysHistoricalDataTest extends TestCase
     private $db;
     private $gatewayHistoricalData;
 
-    public function testgetHistoricalDataBookingsPerLectureFound()
+    protected function setUp(): void
     {
         $this->db = new SQLite3("./tests/dbStatistics.db");
+    }
+
+    public function testgetHistoricalDataBookingsPerLectureFound()
+    {
         $filterTime = "L.idLesson";
         $filterCourse = "L.idCourse";
         $type = "1";
         $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
         $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type);
         $this->assertIsArray($result);
+        $this->assertCount(70, $result);
     }
 
     public function testgetHistoricalDataBookingsPerMonthFound()
     {
-        $this->db = new SQLite3("./tests/dbStatistics.db");
         $filterTime = "year,monthOfYear";
         $filterCourse = "L.idCourse";
         $type = "1";
         $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
         $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type);
         $this->assertIsArray($result);
+        $this->assertCount(3, $result);
     }
 
     public function testgetHistoricalDataBookingsPerWeekFound()
     {
-        $this->db = new SQLite3("./tests/dbStatistics.db");
         $filterTime = "year_month_week";
         $filterCourse = "L.idCourse";
         $type = "1";
         $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
         $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type);
         $this->assertIsArray($result);
+        $this->assertCount(9, $result);
     }
 
     public function testgetHistoricalDataBookingsNotFound()
@@ -47,6 +52,56 @@ class GatewaysHistoricalDataTest extends TestCase
         $type = "1";
         $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
         $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type);
+        $this->assertEquals(0, $result);
+    }
+
+    public function testgetHistoricalDataBookingsPerLecturePerTeacherFound()
+    {
+        $filterTime = "L.idLesson";
+        $filterCourse = "L.idCourse";
+        $type = "1";
+        $teacherId = "2";
+        $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
+        $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type, $teacherId);
+        $this->assertIsArray($result);
+        $this->assertCount(70, $result);
+    }
+
+    public function testgetHistoricalDataBookingsPerMonthPerTeacherFound()
+    {
+
+        $filterTime = "year,monthOfYear";
+        $filterCourse = "L.idCourse";
+        $type = "1";
+        $teacherId = "2";
+        $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
+        $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type, $teacherId);
+        $this->assertIsArray($result);
+        $this->assertCount(3, $result);
+    }
+
+    public function testgetHistoricalDataBookingsPerWeekPerTeacherFound()
+    {
+
+        $filterTime = "year_month_week";
+        $filterCourse = "L.idCourse";
+        $type = "1";
+        $teacherId = "2";
+        $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
+        $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type, $teacherId);
+        $this->assertIsArray($result);
+        $this->assertCount(9, $result);
+    }
+
+    public function testgetHistoricalDataBookingsPerTeacherNotFound()
+    {
+        $this->db = new SQLite3("./tests/dbForTesting.db");
+        $filterTime = "year_month_week";
+        $filterCourse = "L.idCourse";
+        $type = "1";
+        $teacherId = "2";
+        $this->gatewayHistoricalData = new Server\api\GatewaysHistoricalData($this->db);
+        $result = $this->gatewayHistoricalData->getHistoricalDataBookings($filterTime, $filterCourse, $type, $teacherId);
         $this->assertEquals(0, $result);
     }
 }
