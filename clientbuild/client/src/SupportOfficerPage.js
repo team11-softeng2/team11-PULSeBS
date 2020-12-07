3d030008db4acc1ef6bf48be76c2f8a6f0fe083d
Unknown <s267562@studenti.polito.it>
2020-12-07 23:36:48 +0100

    S.O. home page

--- clientbuild/client/src/SupportOfficerPage.js
+++ clientbuild/client/src/SupportOfficerPage.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Row, Container, Button, Col, Card } from "react-bootstrap";
+import { FaTools, FaEdit } from "react-icons/fa";
 
 class SupportOfficerPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            options: [
+                { id: 0, name: "Setup" },
+                { id: 1, name: "Other operations..." },
+                /*{ code: 3, name: "Other"},
+                { code: 4, name: "Other" },
+                { code: 5, name: "Other" },
+                { code: 6, name: "Other" }, */
+            ]
         };
     }
 
@@ -16,34 +24,45 @@ class SupportOfficerPage extends React.Component {
 
     render() {
         return <>
-            <div class="text-center bg-light p-5 mb-5">
+            <div class="text-center mt-5 p-5 mb-5">
                 <h1>Welcome!</h1>
-                <p>Select the service you want to perform.</p>
+                <p>Select the operation you want to perform.</p>
             </div>
-            
             <Container fluid>
-            <Row>
-                    <Service service={{ name: "prova", description: "prova description" }} key={1}></Service>
+                <Row className="justify-content-center align-items-center">
+                    {this.state.options.map((opt) => <CardButton key={opt.id} option={opt}></CardButton>)}
                 </Row>
             </Container>
         </>
     }
-
 }
 
-const Service = (props) => {
+const CardButton = (props) => {
 
-    let { service } = props;
+    let { option } = props;
+    const [hovered, setHovered] = useState(false);
+    const toggleHoverTrue = () => setHovered(true);
+    const toggleHoverFalse = () => setHovered(false);
 
-    return <Col xs="12" sm="6" md="4" lg="3">
-        <Card>
-            <Card.Body>
-                <Card.Title>{service.name}</Card.Title>
-                <Card.Text>{service.description}</Card.Text>
-                <Button variant="primary">BUTTON</Button>
-            </Card.Body>
-        </Card>
-    </Col>
+    return <>
+        <Col lg="2">
+            <Card
+                className={"text-center bg-white" + (hovered ? " shadow" : "")}
+                style={{ cursor: "pointer" }}
+                onMouseEnter={toggleHoverTrue}
+                onMouseLeave={toggleHoverFalse}>
+                <Card.Body> {
+                    (option.id === 0) ?
+                        <FaTools size={32} className="mt-3 mb-4" /> :
+                        (option.id === 1) ?
+                            <FaEdit size={32} className="mt-3 mb-4" /> :
+                            // default
+                            <FaEdit size={32} className="mt-3 mb-4" />}
+                    <Card.Text>{option.name}</Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    </>
 }
 
 export default SupportOfficerPage;
