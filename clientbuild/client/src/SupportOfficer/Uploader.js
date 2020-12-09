fc9c162434a25e2b9b05ab7fe5894045473f6099
Unknown <s267562@studenti.polito.it>
2020-12-09 13:59:29 +0100

    Uploader component

--- clientbuild/client/src/SupportOfficer/Uploader.js
+++ clientbuild/client/src/SupportOfficer/Uploader.js
@@ -1,18 +1,18 @@
 import React, { useState, Component } from 'react'
+import Dropzone from "./Dropzone"
 import './Uploader.css'
-import { FaFileUpload, FaFileAlt } from "react-icons/fa";
 
 class Uploader extends Component {
   constructor(props) {
     super(props);
     this.state = {
       file: null,
-      uploading: false,
-      uploadProgress: {},
+      fileType: null,
       successfullUploaded: false
     };
 
     this.onFileAdded = this.onFileAdded.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   onFileAdded(file) {
@@ -22,124 +22,20 @@ class Uploader extends Component {
   }
 
   render() {
-    return (
-      <div className="Upload">
-        <span className="Title">Upload File</span>
-        <div className="Content">
-          <div>
-            <Dropzone
-              onFileAdded={this.onFileAdded}
-              disabled={this.state.uploading || this.state.successfullUploaded}
-              file={this.state.file}
-            />
-          </div>
-          <div className="File my-auto">
-            <span className="Filename">{this.state.file !== null ? this.state.file.name : ""}</span>
-          </div>
-        </div>
-        <div className="Actions">
-          {this.renderActions()}
-        </div>
-      </div>
+    return (<>
+                <Dropzone
+                  onFileAdded={this.onFileAdded}
+                  disabled={false}
+                  file={this.state.file}
+                />
+    </>
     );
   }
 
-  renderActions() {
-    if (this.state.successfullUploaded) {
-      return (
-        <button
-          onClick={() =>
-            this.setState({ file: null, successfullUploaded: false })
-          }
-        >
-          Clear
-        </button>
-      );
-    } else {
-      return (
-        <button
-          disabled={this.state.file === null || this.state.uploading}
-          onClick={this.uploadFile}
-        >
-          Upload
-        </button>
-      );
-    }
+  handleChange(event) {
+    this.setState({ fileType: event.target.value });
   }
 
-}
-
-
-const Dropzone = (props) => {
-
-  const [hightlight, setHightlight] = useState(false);
-  let fileInputRef = React.createRef();
-
-  const openFileDialog = () => {
-    if (props.disabled) return;
-    fileInputRef.current.click();
-  }
-
-  const onFileAdded = (event) => {
-    if (props.disabled) return;
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-
-      if (props.onFileAdded) {
-        props.onFileAdded(file);
-      }
-    }
-  }
-
-  const onDragOver = (event) => {
-    event.preventDefault();
-    if (props.disabed) return;
-    setHightlight(true);
-  }
-
-  const onDragLeave = (event) => {
-    setHightlight(false);
-  }
-
-  const onDrop = (event) => {
-    event.preventDefault();
-    if (props.disabed) return;
-    if (event.dataTransfer.files.length > 0) {
-      const file = event.dataTransfer.files[0];
-      if (file.type === "text/csv") {
-        if (props.onFileAdded) {
-          props.onFileAdded(file);
-        }
-      }
-    }
-    setHightlight(false);
-  }
-
-  return <>
-    <div
-      className={`Dropzone ${hightlight ? "Highlight" : ""}`}
-      onDragOver={onDragOver}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-      onClick={openFileDialog}
-      style={{ cursor: props.disabled ? "default" : "pointer" }}
-    >
-      <input
-        ref={fileInputRef}
-        className="FileInput"
-        type="file"
-        accept=".csv"
-        onChange={onFileAdded}
-      />
-      {
-        props.file === null ? 
-        <FaFileUpload className="UploadIcon my-3" />
-        :
-        <FaFileAlt className="FileIcon my-3" />
-      }
-      <span className="Filename">{ props.file !== null ? props.file.name : "Upload File" }</span>
-    </div>
-  </>
 }
 
 export default Uploader;
