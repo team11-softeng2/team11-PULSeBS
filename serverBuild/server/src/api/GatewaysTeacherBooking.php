d5bc3327209d4ff9a0abcdabfdad5c6d59b6dc6d
CalogeroPisano <calo19296@gmail.com>
2021-01-05 16:53:21 +0100

    Update GatewaysTeacherBooking.php

--- serverBuild/server/src/api/GatewaysTeacherBooking.php
+++ serverBuild/server/src/api/GatewaysTeacherBooking.php
@@ -12,13 +12,14 @@ class GatewaysTeacherBooking extends Gateways
 
     public function findBookedStudentsForLecture($id)
     {
-        $sql = "select U.idUser, U.name from booking B join users U where B.idUser=U.idUser and B.idLesson=" . $id . " and U.role='student' and B.active=1 and B.isWaiting=0;";
+        $sql = "select U.idUser, U.name, B.idBooking from booking B join users U where B.idUser=U.idUser and B.idLesson=" . $id . " and U.role='student' and B.active=1 and B.isWaiting=0;";
         $result = $this->db->query($sql);
         $data = array();
         while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
             $subArray = array(
                 "idUser" => $row['idUser'],
                 "name" => $row['name'],
+                "idBooking" => $row['idBooking']
             );
             $data[] = $subArray;
         }
